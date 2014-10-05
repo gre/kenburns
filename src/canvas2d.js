602b69235b33a4337070cc628fdd0f818d68016e
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-10-05 22:06:07 +0200

    use drawImageNormalized

--- src/canvas2d.js
+++ src/canvas2d.js
@@ -1,3 +1,4 @@
+var drawImageNormalized = require("draw-image-normalized");
 
 // Canvas 2D implementation
 
@@ -6,39 +7,16 @@ function KenBurnsCanvas2dTrait (canvas2d) {
   this.ctx = canvas2d.getContext("2d");
 }
 KenBurnsCanvas2dTrait.prototype = {
-  draw: function (image, bound) {
+  draw: function (image, rect) {
     var viewport = [ 0, 0, this.canvas.width, this.canvas.height ];
     var ctx = this.ctx;
     ctx.fillStyle = "rgb("+this.rgb.map(function(x){ return x * 255; })+")";
     ctx.fillRect.apply(ctx, viewport);
-    var params = normalizeDrawingParams(bound, viewport);
-    ctx.drawImage.apply(ctx, [ image ].concat(params));
+    drawImageNormalized.apply(null, [ ctx, image ].concat(rect).concat(viewport));
   },
   getViewport: function () {
     return this.canvas;
   }
 };
-
-function normalizeDrawingParams (bound, viewport) {
-  // TODO implement for Firefox (more restrictive on out of bounds)
-  // TODO rounding?
-  // TODO external module
-  var params = bound.concat(viewport);
-  /*
-  if (params[0] < 0) {
-    params[4] -= params[0];
-    //params[2] += params[0];
-    params[6] += params[0];
-    params[0] = 0;
-  }
-  if (params[1] < 0) {
-    params[5] -= params[1];
-    //params[3] += params[1];
-    params[7] += params[1];
-    params[1] = 0;
-  }
-  */
-  return params;
-}
 
 module.exports = KenBurnsCanvas2dTrait;
